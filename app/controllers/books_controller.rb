182f1516107be284cfe8a0698ece3ceacbcb00bf
ayana0120 <keichan0501.ayana@gmail.com>
2020-09-01 16:46:52 +0900

    [Fix]コード修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,7 @@ class BooksController < ApplicationController
   end
 
   def update
-    if @book.update()
+    if @book.update(book_params)
       redirect_to book_path(@book), notice: "successfully updated book!"
     else
       render 'edit'
